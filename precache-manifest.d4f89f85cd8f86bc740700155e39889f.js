ce00961df33ee487e1ab46b77e5f037686ea69ed
Duy Phan <ducduymm0@gmail.com>
2021-07-12 16:43:07 +0700

    Updates

---
+++ precache-manifest.d4f89f85cd8f86bc740700155e39889f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "388e2675dcd7221ec9be",
+    "url": "/static/js/0.388e2675.chunk.js"
+  },
+  {
+    "revision": "32450c41f93a8a6ecd24",
+    "url": "/static/js/1.32450c41.chunk.js"
+  },
+  {
+    "revision": "266e46242c8689ba3288",
+    "url": "/static/js/2.266e4624.chunk.js"
+  },
+  {
+    "revision": "b797309b8c5253049c70",
+    "url": "/static/js/3.b797309b.chunk.js"
+  },
+  {
+    "revision": "d6cee67c595bedfe995f",
+    "url": "/static/js/4.d6cee67c.chunk.js"
+  },
+  {
+    "revision": "7ff645d8a6fa3430b080",
+    "url": "/static/js/main.7ff645d8.chunk.js"
+  },
+  {
+    "revision": "b4a04066490ff0a88581",
+    "url": "/static/js/runtime~main.b4a04066.js"
+  },
+  {
+    "revision": "07b5d8046ae4a5a6a691",
+    "url": "/static/css/7.e521d06b.chunk.css"
+  },
+  {
+    "revision": "07b5d8046ae4a5a6a691",
+    "url": "/static/js/7.07b5d804.chunk.js"
+  },
+  {
+    "revision": "2d8bfdf69572c522c4e3",
+    "url": "/static/css/8.b830f237.chunk.css"
+  },
+  {
+    "revision": "2d8bfdf69572c522c4e3",
+    "url": "/static/js/8.2d8bfdf6.chunk.js"
+  },
+  {
+    "revision": "387516853efa09dbd7d8",
+    "url": "/static/css/9.a0101b3e.chunk.css"
+  },
+  {
+    "revision": "387516853efa09dbd7d8",
+    "url": "/static/js/9.38751685.chunk.js"
+  },
+  {
+    "revision": "ad5a7dff5f5b16619ea7",
+    "url": "/static/js/10.ad5a7dff.chunk.js"
+  },
+  {
+    "revision": "85b04a75f1a94142c27d",
+    "url": "/static/js/11.85b04a75.chunk.js"
+  },
+  {
+    "revision": "b089f67e14975c542434",
+    "url": "/static/js/12.b089f67e.chunk.js"
+  },
+  {
+    "revision": "bf24e90d8d5936314cbc",
+    "url": "/static/js/13.bf24e90d.chunk.js"
+  },
+  {
+    "revision": "64e3c148e50e55915ff5",
+    "url": "/static/js/14.64e3c148.chunk.js"
+  },
+  {
+    "revision": "66a031f87dbca5b3270001bafb23ffd9",
+    "url": "/index.html"
+  }
+];
